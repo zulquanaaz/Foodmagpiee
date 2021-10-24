a056166e41e992ca271845397134301af9723fe2
Gurpreetgss <90996753+Gurpreetgss@users.noreply.github.com>
2021-10-24 13:09:12 +0530

    Modified Request Food activity class

--- app/src/main/java/com/foodrecipe/RequestFoodActivity.java
+++ app/src/main/java/com/foodrecipe/RequestFoodActivity.java
@@ -25,7 +25,7 @@ import java.util.regex.Pattern;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-
+//Request Food Activity Class
 public class RequestFoodActivity extends AppCompatActivity {
 
     EditText etname, etemail, etphone, etmessage;
