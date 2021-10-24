8b5b4cf4ce0f3a5728148c6664d5b7ef77fe7c5d
Gurpreetgss <90996753+Gurpreetgss@users.noreply.github.com>
2021-10-24 12:01:30 +0530

    Add Charity activity files

---
+++ app/src/main/java/com/foodrecipe/AddCharityActivity.java
@@ -0,0 +1,193 @@
+package com.foodmagpie;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.foodmagpie.api.ApiService;
+import com.foodmagpie.model.ResponseData;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import pub.devrel.easypermissions.EasyPermissions;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class AddCharityActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks  {
+
+    private static final int REQUEST_GALLERY_CODE = 200;
+    private static final int READ_REQUEST_CODE = 300;
+    private static final String SERVER_PATH = "http://covidinformation.live/";
+    EditText etaddress,etname,etphone;
+    private Uri uri;
+    Button btnadd,btnimageupload;
+    ProgressDialog progress;
+    int mYear, mMonth, mDay;
+    String DAY, MONTH, YEAR;
+    private static final String TAG = AddCharityActivity.class.getSimpleName();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_charity);
+        getSupportActionBar().setTitle("Add Organisation");
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        etname=(EditText)findViewById(R.id.etname);
+        etphone=(EditText)findViewById(R.id.etphone);
+        etaddress=(EditText)findViewById(R.id.etaddress);
+
+        btnimageupload=(Button) findViewById(R.id.btnimageupload);
+        btnimageupload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent openGalleryIntent = new Intent(Intent.ACTION_PICK);
+                openGalleryIntent.setType("image/*");
+                startActivityForResult(openGalleryIntent, REQUEST_GALLERY_CODE);
+            }
+        });
+
+        btnadd=(Button)findViewById(R.id.btnadd);
+        btnadd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (etphone.getText().toString().isEmpty()) {
+                    Toast.makeText(AddCharityActivity.this, "Enter Phone", Toast.LENGTH_SHORT).show();
+                    return;
+                } else if (etname.getText().toString().isEmpty()) {
+                    Toast.makeText(AddCharityActivity.this, "Enter Name", Toast.LENGTH_SHORT).show();
+                    return;
+                } else if (etaddress.getText().toString().isEmpty()) {
+                    Toast.makeText(AddCharityActivity.this, "Enter Address", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                else {
+                    addHotel();
+                }
+
+            }
+        });
+
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, AddCharityActivity.this);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == REQUEST_GALLERY_CODE && resultCode == Activity.RESULT_OK){
+            uri = data.getData();
+            if(EasyPermissions.hasPermissions(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                String filePath = getRealPathFromURIPath(uri, AddCharityActivity.this);
+                file = new File(filePath);
+
+            }else{
+                EasyPermissions.requestPermissions(this, getString(R.string.read_file), READ_REQUEST_CODE, Manifest.permission.READ_EXTERNAL_STORAGE);
+            }
+        }
+    }
+    private String getRealPathFromURIPath(Uri contentURI, Activity activity) {
+        Cursor cursor = activity.getContentResolver().query(contentURI, null, null, null, null);
+        if (cursor == null) {
+            return contentURI.getPath();
+        } else {
+            cursor.moveToFirst();
+            int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+            return cursor.getString(idx);
+        }
+    }
+    File file;
+    @Override
+    public void onPermissionsGranted(int requestCode, List<String> perms) {
+        if(uri != null){
+            String filePath = getRealPathFromURIPath(uri, AddCharityActivity.this);
+            file = new File(filePath);
+        }
+    }
+    @Override
+    public void onPermissionsDenied(int requestCode, List<String> perms) {
+        Log.d(TAG, "Permission has been denied");
+
+    }
+
+    private void addHotel() {
+        progress = new ProgressDialog(AddCharityActivity.this);
+        progress.setTitle("Loading");
+        progress.show();
+        SharedPreferences sharedPreferences = getSharedPreferences(Utils.SHREF, Context.MODE_PRIVATE);
+        String email = sharedPreferences.getString("user_name", "def-val");
+        Map<String, String> map = new HashMap<>();
+
+        map.put("name", etname.getText().toString());
+        map.put("phone", etphone.getText().toString());
+        map.put("address", etaddress.getText().toString());
+        map.put("email", email);
+
+        RequestBody mFile = RequestBody.create(MediaType.parse("image/*"), file);
+        MultipartBody.Part fileToUpload = MultipartBody.Part.createFormData("file", file.getName(), mFile);
+        RequestBody filename = RequestBody.create(MediaType.parse("text/plain"), file.getName());
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(SERVER_PATH)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+
+        ApiService api = retrofit.create(ApiService.class);
+        Call<ResponseData> fileUpload = api.addcharity(fileToUpload, map);
+
+        fileUpload.enqueue(new Callback<ResponseData>() {
+            @Override
+            public void onResponse(Call<ResponseData> call, Response<ResponseData> response) {
+                progress.dismiss();
+                Toast.makeText(AddCharityActivity.this, "Hotel Added successfully. ", Toast.LENGTH_LONG).show();
+                startActivity(new Intent(AddCharityActivity.this,CharityhomeActivity.class));
+                finish();
+            }
+
+            @Override
+            public void onFailure(Call<ResponseData> call, Throwable t) {
+                progress.dismiss();
+                Toast.makeText(AddCharityActivity.this, "Error" + t.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
